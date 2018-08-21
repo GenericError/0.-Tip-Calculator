5b65dd0a16c100e01049b98f454e014d3d75355c
Luke Stewart <stewartluke21@yahoo.com>
2018-08-21 20:51:23 +1000

    Force the user to enter valid numbers
    Challenges #1 and #2

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,5 +1,11 @@
-bill_amount = float(input("What is the bill amount? "))
-tip_percentage = float(input("What is the tip amount? "))
+while True:
+    try:
+        bill_amount = float(input("What is the bill amount? "))
+        tip_percentage = float(input("What is the tip amount? "))
+        break
+    except:
+        print("Your input must be a valid number!")
+        continue
 
 tip_amount = bill_amount * ( tip_percentage / 100 )
 bill_with_tip = bill_amount + tip_amount
