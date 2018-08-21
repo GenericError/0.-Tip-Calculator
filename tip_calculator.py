d784ac68d5b2312b484f208df9b553fb532876f4
Luke Stewart <stewartluke21@yahoo.com>
2018-08-21 20:49:13 +1000

    Created Tip Calculator

---
+++ tip_calculator.py
@@ -0,0 +1,8 @@
+bill_amount = float(input("What is the bill amount? "))
+tip_percentage = float(input("What is the tip amount? "))
+
+tip_amount = bill_amount * ( tip_percentage / 100 )
+bill_with_tip = bill_amount + tip_amount
+
+print( f"The tip is ${tip_amount:.2f}" )
+print( f"The total bill is ${bill_with_tip:.2f}" )
